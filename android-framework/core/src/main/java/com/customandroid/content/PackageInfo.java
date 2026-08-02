51ec68e62132ba38b80e6d461d5785b0e5a42a35
carjam1200-jpg <carjam1200@gmail.com>
2026-08-02 12:24:06 -0400

    Add PackageInfo framework class

---
+++ android-framework/core/src/main/java/com/customandroid/content/PackageInfo.java
@@ -0,0 +1,11 @@
+package com.customandroid.content;
+
+public class PackageInfo {
+    public String packageName;
+    public String versionName;
+
+    public PackageInfo(String packageName, String versionName) {
+        this.packageName = packageName;
+        this.versionName = versionName;
+    }
+}
