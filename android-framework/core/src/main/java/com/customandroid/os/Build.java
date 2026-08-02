4ef3a3917828e8bd1a8b230df4c91187df7083ac
carjam1200-jpg <carjam1200@gmail.com>
2026-08-02 12:22:56 -0400

    Add CustomAndroid framework Build API

---
+++ android-framework/core/src/main/java/com/customandroid/os/Build.java
@@ -0,0 +1,8 @@
+package com.customandroid.os;
+
+public final class Build {
+    public static final String VERSION = "0.1";
+    public static final String NAME = "CustomAndroid Framework";
+
+    private Build() {}
+}
